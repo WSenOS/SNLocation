eab2611ef5072a096ef52e2751f6a1974b172307
w_sen <wangsenwd@hotmail.com>
2016-01-16 15:01:56 +0800

    Initial Commit

---
+++ SNLocation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SNLocation
+//
+//  Created by wangsen on 16/1/16.
+//  Copyright © 2016年 wangsen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SNLocation/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SNLocation
+//
+//  Created by wangsen on 16/1/16.
+//  Copyright © 2016年 wangsen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
